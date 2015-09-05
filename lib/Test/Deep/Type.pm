bfd71c3ec71ceeba0e78ba8efcb79834d3b3fa92
Karen Etheridge <ether@cpan.org>
2015-09-05 11:39:19 -0700

    this section is now generated by my weaver bundle

--- lib/Test/Deep/Type.pm
+++ lib/Test/Deep/Type.pm
@@ -189,13 +189,6 @@ renderGot
 Regular strings describing a type under a particular system
 (e.g. L<Moose>, L<Params::Validate>) are not currently supported.
 
-=head1 SUPPORT
-
-=for stopwords irc
-
-Bugs may be submitted through L<https://rt.cpan.org/Public/Dist/Display.html?Name=Test-Deep-Type>.
-I am also usually active on irc, as 'ether' at L<irc://irc.perl.org>.
-
 =head1 SEE ALSO
 
 =for :list
